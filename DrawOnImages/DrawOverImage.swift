8f48cc4ff7f579183b5cb7cfec53a4a8b8ef958e
Jayant Arora <jaistudy1996@gmail.com>
2018-03-05 13:16:04 -0500

    add documentation for DrawOverImage and remove unwraping issues

--- DrawOnImages/DrawOverImage.swift
+++ DrawOnImages/DrawOverImage.swift
@@ -24,13 +24,13 @@ class DrawOverImage: UIViewController {
     @IBAction func DoneEditing(_ sender: UIBarButtonItem) {
         
         // Merge two images before exiting view controller
-        
-        //mainImage.image = mergedImageWith(frontImage: editsForImage.image, backgroundImage: mainImage.image)
-        
-        //editsForImage = nil
-        saveImage()
-        delgate?.doneEditing(image: mainImage.image!) // TODO: remove implicit unwrap
-//        mergeImageAsync()
+        mergeImage()
+        editsForImage = nil // free unnecessary memory usage.
+        guard let imageForDelegate = mainImage.image else {
+            dismiss(animated: true, completion: nil)
+            return
+        }
+        delgate?.doneEditing(image: imageForDelegate)
         dismiss(animated: true, completion: nil)
     }
     
@@ -43,7 +43,6 @@ class DrawOverImage: UIViewController {
     var oldTouchPoint: CGPoint? // the last time when the screen was touch
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         setUpImage()
     }
@@ -54,15 +53,15 @@ class DrawOverImage: UIViewController {
     }
     
     // MARK: Setup Initial image
+    /**
+     Set up background image to be merged when the edits are complete
+    */
     private func setUpImage() {
         mainImage.image = imageForMainImage
     }
     
-    
-    
     // MARK: Setup touch gestures
     @IBAction func tapGesture(_ gesture: UITapGestureRecognizer) {
-        
         if (gesture.state == .recognized) {
             print("Initial Touch Gesture")
         }
@@ -77,14 +76,22 @@ class DrawOverImage: UIViewController {
         }
         
         if (gesture.state != .ended) {
-            drawLine(from: oldTouchPoint!, to: currentTouchLoc) // FIX !!!!
+            if (oldTouchPoint != nil){
+                drawLine(from: oldTouchPoint!, to: currentTouchLoc)
+            }
         }
         
         oldTouchPoint = currentTouchLoc
     }
     
     // MARK: Draw line between two vertcies
-    
+    /**
+     Connect two points using a line
+     
+     - parameters:
+        - from: The starting point for line to be drawn
+        - to: The ending point for line to be drawn
+    */
     private func drawLine(from: CGPoint, to: CGPoint) {
         let size = editsForImage.frame.size
         
@@ -110,72 +117,20 @@ class DrawOverImage: UIViewController {
 
     }
     
-    private func saveImage() {
+    /**
+     Merge two images together (bacground and foreground)
+    */
+    private func mergeImage() {
         let renderer = UIGraphicsImageRenderer(size: mainImage.frame.size)
         
         let img = renderer.image { ctx in
             let rectangle = CGRect(x: 0, y: 0, width: mainImage.frame.width, height: mainImage.frame.height)
             ctx.cgContext.addRect(rectangle)
-//            ctx.cgContext.saveGState()
-//            ctx.cgContext.concatenate(ctx.cgContext.userSpaceToDeviceSpaceTransform)
-//            ctx.cgContext.rotate(by: CGFloat.pi/2)
-//            ctx.cgContext.translateBy(x: 0, y: mainImage.frame.width)
-            //
-//            ctx.cgContext.translateBy(x: rectangle.midX, y: rectangle.midY)
-//            ctx.cgContext.scaleBy(x: 1, y: -1)
-//            ctx.cgContext.rotate(by: (-CGFloat.pi/2))
-//            ctx.cgContext.translateBy(x: -rectangle.midX, y: -rectangle.midY)
-//            ctx.cgContext.draw(mainImage.image!.cgImage!, in: rectangle)
-            
-//            ctx.cgContext.translateBy(x: rectangle.midX, y: rectangle.midY)
-//            ctx.cgContext.scaleBy(x: 1, y: -1)
-//            ctx.cgContext.rotate(by: (CGFloat.pi/2))
-//            ctx.cgContext.translateBy(x: -rectangle.midX, y: -rectangle.midY)
-//            ctx.cgContext.draw(editsForImage.image!.cgImage!, in: rectangle)
-            //
-            
             mainImage.image?.draw(in: rectangle)
             editsForImage.image?.draw(in: rectangle)
         }
         mainImage.image = img
     }
-    
-    /*
-    private func mergedImageWith(frontImage:UIImage?, backgroundImage: UIImage?) -> UIImage{
-        
-        if (backgroundImage == nil) {
-            return frontImage!
-        }
-        let size = backgroundImage?.size
-//        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        
-//        UIGraphicsBeginImageContextWithOptions(size!, false, 1)
-        
-//        backgroundImage?.draw(in: CGRect.init(x: 0, y: 0, width: (size?.width)!, height: (size?.height)!))
-//        frontImage?.draw(in: CGRect.init(x: 0, y: 0, width: (size?.width)!, height: (size?.height)!).insetBy(dx: (size?.width)! * 0.01, dy: (size?.height)! * 0.01))
-//
-//        backgroundImage?.draw(at: CGPoint(x: 0, y: 0))
-//        frontImage?.draw(in: CGRect(x: 0, y: 0, width: (size?.width)!, height: (size?.height)!)) // TODO: remove force unwrap
-//        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-//        UIGraphicsEndImageContext()
-        
-        
-//        return newImage
-        return img
-        
-    }
-    
-    private func mergeImageAsync() {
-        DispatchQueue.global(qos: .background).async {
-            autoreleasepool(invoking: {
-                let tempImage = self.mergedImageWith(frontImage: self.editsForImage.image, backgroundImage: self.mainImage.image)
-                self.mainImage.image = tempImage
-                self.delgate?.doneEditing(image: self.mainImage.image!)
-                print("dispatch queue done")
-            })
-        }
-    }
-    */
 }
 
 extension DrawOverImage: StoryboardInitializable {
